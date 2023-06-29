d41c2e010b5bf4f214840fb74469666316237fb9
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-06-29 17:28:20 +0200

    Add exercises 2 and 3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
     res.send("Hola, mundo!");
 });
 
-//---------------------- Ejercicio 1 ---------------------------//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Ejercicio 1 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
 
 // Conexión a la DDBB
 const db = mysql.createConnection({
@@ -59,10 +59,72 @@ app.post("/products", (req, res) => {
         if (err) throw err;
         console.log("Producto agregado:", result);
 
-        // Enviar respuesta al cliente
-        res.status(201).json({ message: "Producto agregado correctamente" });
+        // Envia un mensajito para confirmar
+        res.status(201).json({ message: "Se agregó el producto" });
     });
 });
+
+// Endpoint que agrega una categoría nueva
+app.post("/categories", (req, res) => {
+    const { name_cat } = req.body;
+
+    const sql = "INSERT INTO category (name_cat) VALUES (?)";
+    db.query(sql, [name_cat], (err, result) => {
+        if (err) throw err;
+        console.log("Categoría agregada:", result);
+
+        // Otro mensajito de confirmación
+        res.status(201).json({ message: "Se agregó la categoría" });
+    });
+});
+
+// Endpoint que enlaza la categoría con el producto
+app.post("/product-category", (req, res) => {
+    const { productId, categoryId } = req.body;
+
+    // Acá hacemos la verificación para enlazar correctamente
+    const sql = "INSERT INTO productCat (product_id, category_id) VALUES (?, ?)";
+    db.query(sql, [productId, categoryId], (err, result) => {
+        if (err) throw err;
+        console.log("Enlace creado:", result);
+
+        // La confirmación
+        res.status(201).json({ message: "El enlace se creó re copado" });
+    });
+});
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Ejercicio 3 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
+
+// Endpoint que actualiza el producto
+app.put("/products/:id", (req, res) => {
+    const productId = req.params.id;
+    const { name, description, price } = req.body;
+
+    const sql = "UPDATE product SET name=?, description=?, price=? WHERE idproduct=?";
+    db.query(sql, [name, description, price, productId], (err, result) => {
+        if (err) throw err;
+        console.log("Producto actualizado:", result);
+
+        // Mensajito confirm
+        res.status(200).json({ message: "Se actualizó el producto" });
+    });
+});
+
+// Endpoint donde actualizamos una categoría
+app.put("/categories/:id", (req, res) => {
+    const categoryId = req.params.id;
+    const { name_cat } = req.body;
+
+    const sql = "UPDATE category SET name_cat=? WHERE idcategory=?";
+    db.query(sql, [name_cat, categoryId], (err, result) => {
+        if (err) throw err;
+        console.log("Categoría actualizada:", result);
+
+        // Mensajito confirm
+        res.status(200).json({ message: "La categoría se actualizó" });
+    });
+});
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Ejercicio 3 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-//
 
 // Iniciar el servidor
 app.listen(port, () => {
